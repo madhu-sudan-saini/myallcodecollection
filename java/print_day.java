20e7c1fe8bb4a46b7d0bbc55f5fd0e01462d0e7c
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-04-26 09:14:06 +0530

    Create print_day.java

---
+++ java/print_day.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+class day
+  {
+    public static void main(String args[])
+    {
+      int num;
+      num=Integer.parseInt(JOptionPane.showInputDialog("enter a number"));
+
+      if(num==1)
+      {
+        System.out.println("monday");
+      }
+      else if(num==2)
+      {
+        System.out.println("tuesday");
+      }
+            else if(num==3)
+      {
+        System.out.println("wednesday");
+      }
+        else if(num==4)
+      {
+        System.out.println("thrusday");
+      }
+            else if(num==5)
+      {
+        System.out.println("friday");
+      }
+            else if(num==6)
+      {
+        System.out.println("saturday");
+      }
+            else if(num==7)
+      {
+        System.out.println("sunday");
+      }
+    }
+  }   
+      
