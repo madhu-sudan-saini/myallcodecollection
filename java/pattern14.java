77da0d5924dd36238b736432937dce9c1d0cc61a
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-03 18:15:30 +0530

    Create pattern14.java
    pattern
    
        $
       $*$
      $*$*$
     $*$*$*$
    $*$*$*$*$

---
+++ java/pattern14.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+class Demo
+{
+	public static void main(String args[])
+	{
+		int i,j,sp;
+		for(i=1;i<=5;i++)
+		{
+			for(sp=1;sp<=5-i;sp++)
+			{	
+				System.out.print(" ");
+			}
+			for(j=1;j<=(2*i-1);j++)
+			{
+				if(j%2==0)
+				{	
+				System.out.print("*");
+				}
+				else
+				{
+				System.out.print("$");
+				}
+			}
+			System.out.println();
+			}
+			
+		}
+}
