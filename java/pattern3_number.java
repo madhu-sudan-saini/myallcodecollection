f96273f8fd56d18fe2f15f40121a9a6aec8a7d40
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-03 17:24:51 +0530

    Create pattern3_number.java
    pattern of number just  looking like
    
    1
    22
    333
    4444
    55555

---
+++ java/pattern3_number.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+class Demo
+{
+	public static void main(String args[])
+	{
+		int i,j;
+		for(i=1;i<6;i++)
+		{
+			for(j=1;j<=i;j++)
+			{	
+
+				System.out.print(i);
+			}
+			
+			System.out.print("\n");
+		}
+	}
+}
