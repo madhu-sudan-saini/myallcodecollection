5734fe5cc7a56147c7ee3c97aa96025980627902
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-03 17:38:45 +0530

    Create pattern5.java
    pattern 
    
    54321
    5432
    543
    54
    5

---
+++ java/pattern5.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+class Demo
+{
+	public static void main(String args[])
+	{
+		int i,j,k;
+		for(i=1;i<6;i++)
+		{
+			k=5;
+			for(j=1;j<=6-i;j++)
+			{	
+
+				System.out.print(k);
+				k--;
+			}
+			
+			System.out.print("\n");
+		}
+	}
+}
