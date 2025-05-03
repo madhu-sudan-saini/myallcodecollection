8891c4353a0c1411b40106a3a3d7fd94f43b714a
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-03 16:27:21 +0530

    Create aremstrong_number.cpp

---
+++ c++/aremstrong_number.cpp
@@ -0,0 +1,25 @@
+import javax.swing.*;
+class Demo
+{
+	public static void main(String args[])
+	{
+		int num,sum=0,digit;
+		num=Integer.parseInt(JOptionPane.showInputDialog("enter a number"));
+		int k=num;
+		while(num>0)
+		{
+			digit=num%10;
+			int cube=digit*digit*digit;
+			sum=sum+cube;
+			num=num/10;
+		}
+		if(sum==k)
+		{
+		System.out.println("armstrong :");
+		}
+		else
+		{
+		System.out.println("not armstrong");
+		}
+	}
+}
