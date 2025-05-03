a71c67b7df2ff64bde173254ac8abc091894c4a8
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-03 17:06:56 +0530

    Create pattern1.java
    pattern
    
    *
    **
    ***
    ****
    *****
    ******
    *******

---
+++ java/pattern1.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+class Demo
+{
+	public static void main(String args[])
+	{
+		int i,j;
+		for(i=1;i<6;i++)
+		{
+			for(j=1;j<=i;j++)
+			{	
+
+				System.out.print("*");
+			}
+			
+			System.out.print("\n");
+		}
+	}
+}
