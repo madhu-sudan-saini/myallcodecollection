0c6bc9691c365bf77e7f4866cdc97951582902f6
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-03 17:53:20 +0530

    Create pattern8.java
    patten
    
        *
       **
      ***
     ****
    *****

---
+++ java/pattern8.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+class Demo
+{
+	public static void main(String args[])
+	{
+		int i,j,sp;
+		for(i=1;i<6;i++)
+		{
+			for(sp=1;sp<=5-i;sp++)
+			{	
+				System.out.print(" ");
+			}
+				for(j=1;j<=i;j++)
+				{	
+					System.out.print("*");
+				}
+			System.out.print("\n");
+		}
+	}
+}
