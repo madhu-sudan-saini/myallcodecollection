584d2ee442fb0211ce83bdd1891bceb44653b3dc
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-04 23:47:54 +0530

    Create input_list.py

---
+++ python/input_list.py
@@ -0,0 +1,11 @@
+movie=[]
+
+mov1=input("enter a movie name :")
+mov2=input("enter a movie name :")
+mov3=input("enter a movie name :")
+
+movie.append(mov1)
+movie.append(mov2)
+movie.append(mov3)
+
+print(movie)
