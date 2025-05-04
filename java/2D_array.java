dcc6c85c9181966070981e3ebb00e677acb606d2
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-04 18:36:32 +0530

    Create 2D_array.java

---
+++ java/2D_array.java
@@ -0,0 +1,13 @@
+class First {
+    public static void main(String args[]) {
+        int arr[][] = new int[3][3];
+
+        arr[0][0] = 10;
+        arr[0][1] = 20;
+        arr[0][2] = 30;
+
+        System.out.println(arr[0][0]);
+        System.out.println(arr[0][1]);
+        System.out.println(arr[0][2]);
+    }
+}
