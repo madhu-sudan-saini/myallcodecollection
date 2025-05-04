6817f09b32790c0da165f90a99dd837ed1965c14
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-04 11:16:47 +0530

    Create calculator.py

---
+++ python/calculator.py
@@ -0,0 +1,17 @@
+a=int(input("enter a number :"))
+b=int(input("enter a number :"))
+
+sum=a+b
+print(f"sum is : {sum}")
+
+subtraction=a-b
+print(f"subtraction  is : {subtraction}")
+
+multiple=a*b
+print(f"multiple is : {multiple}")
+
+division=a/b
+print(f"division  is : {division}")
+
+moduls=a%b
+print(f"moduls is : {moduls}")
