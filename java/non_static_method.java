6da9c032571f699f3051dc24f6fb00f0f5eff0d6
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-06 18:03:05 +0530

    Create non_static_method.java

---
+++ java/non_static_method.java
@@ -0,0 +1,14 @@
+class First
+{
+	public  void hello()
+	{
+		System.out.println("hello world");
+	}
+	public static void main(String args[])
+	{
+		First obj=new First();
+		obj.hello();
+
+	}
+}
+	
