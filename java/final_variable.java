63563552aa4a8c8824ec139e322bf742557c0642
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-09 00:01:06 +0530

    Create final_variable.java

---
+++ java/final_variable.java
@@ -0,0 +1,14 @@
+class First {
+    public static void main(String args[]) {
+        final double pie = 3.14;
+        double r, ans;
+
+        r = 5;
+        ans = pie * r * r;
+        System.out.println("Area of circle: " + ans);  
+
+        r = 7;
+        ans = pie * r * r;
+        System.out.println("Area of circle: " + ans);  
+    }
+}
