ffa67cdd58f4fc0a63512705f967d99416205a60
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-09 23:13:34 +0530

    Create compiletime_polemorphism.java

---
+++ java/compiletime_polemorphism.java
@@ -0,0 +1,18 @@
+class Arithmatic
+{
+	public void add(int a,int b)
+	{
+		int ans=a+b;
+		System.out.println("addition is :"+ans);
+	}
+	
+}
+
+class Demo
+{
+	public static void main(String args[])
+	{
+		Arithmatic obj=new Arithmatic();
+		obj.add(10,20);
+	}
+}
