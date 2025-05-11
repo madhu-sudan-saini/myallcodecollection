d068f6cbdd29b0d3c9009794d253f7829df2bb61
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-11 22:31:52 +0530

    Create StringClass_replace.java

---
+++ java/StringClass_replace.java
@@ -0,0 +1,14 @@
+class First
+{
+	public static void main(String args[])
+	{
+	String s="java is a language";
+
+	String ans=s.replace("java","python");
+
+	System.out.println("String is:"+s);
+
+	System.out.println("new String is :"+ans);
+	}
+}
+	
