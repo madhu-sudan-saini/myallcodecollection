149d6ac4eab2d2aebc9c7808029abc00a472617a
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-11 21:13:17 +0530

    Create String_class.java

---
+++ java/String_class.java
@@ -0,0 +1,17 @@
+class Demo
+{
+	public static void main(String args[])
+	{
+		String name=new String();
+		name="madhu sudan saini";
+		int len=name.length();
+		System.out.println("name :"+name);
+		System.out.println("length :"+len);
+		String s="madhu sudan saini";
+		len=s.length();
+		System.out.println("name :"+s);
+		System.out.println("length :"+len);
+	}
+}
+		
+		
