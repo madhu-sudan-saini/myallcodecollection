e47bef307321e7f3bfde4a591cde6f1f73ddbe77
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-11 22:26:42 +0530

    Create StringClass_trim_method.java

---
+++ java/StringClass_trim_method.java
@@ -0,0 +1,15 @@
+class First
+{
+	public static void main(String args[])
+	{
+	String s=" hello ";
+	int len=s.length();
+	
+	System.out.println("String of length :"+len);
+	String ans=s.trim();
+	len=ans.length();
+	System.out.println("String is:"+ans);
+	System.out.println("length is:"+len);
+	}
+}
+	
