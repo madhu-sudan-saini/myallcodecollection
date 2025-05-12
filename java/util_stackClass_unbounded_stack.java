1b699c7f1ab34a5ff6b695f6711695bb77008b07
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-12 23:33:56 +0530

    Create util_stackClass_unbounded_stack.java

---
+++ java/util_stackClass_unbounded_stack.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+class stack
+{
+	public static void main(String args[])
+	{
+		Stack s= new Stack();
+		s.push(10);
+		s.push('m');
+		s.push(30.0);
+		s.push("kamal");
+		
+		System.out.println(s);
+	}
+}
+	
+		
