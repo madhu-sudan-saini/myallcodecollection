7cc035b5c385234547f760a3c75931873d4ce185
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-12 23:13:16 +0530

    Create util_DateClass_setTime.java

---
+++ java/util_DateClass_setTime.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class First
+{
+	public static void main(String args[])
+	{
+		Date d=new Date();
+		d.setHours(8);
+		d.setMinutes(50);
+		d.setSeconds(45);
+		int hours=d.getHours();
+		int minutes=d.getMinutes();
+		int seconds=d.getSeconds();
+		
+		System.out.println(" System set time");
+		System.out.println(hours+"-"+minutes+"-"+seconds);
+	}
+}
+		
