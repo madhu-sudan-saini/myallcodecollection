df0969ebc1cc462e7b7b434f72845d0ecfec8d94
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-12 22:47:49 +0530

    Create StringBufferClass_reverse_method.java

---
+++ java/StringBufferClass_reverse_method.java
@@ -0,0 +1,15 @@
+class First
+{
+	public static void main(String args[])
+	{
+		
+			StringBuffer s2=new StringBuffer("hello world how are you ");
+		
+		System.out.println(s2);
+	
+		s2.reverse();
+		
+		System.out.println(s2);
+  }
+}
+  
