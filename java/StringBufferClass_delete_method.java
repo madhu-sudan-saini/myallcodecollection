46ff4e4c0ca338191901e5e950aeebd2c933f80e
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-12 22:44:56 +0530

    Create StringBufferClass_delete_method.java

---
+++ java/StringBufferClass_delete_method.java
@@ -0,0 +1,15 @@
+
+class First
+{
+	public static void main(String args[])
+	{
+		
+		StringBuffer s1=new StringBuffer("hello world how are you ");
+		
+		System.out.println(s1);
+		
+		s1.insert(12,"ajmer ");
+		
+		System.out.println(s1);
+  }
+}
