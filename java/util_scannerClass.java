52d25035201b7d06236f128d68a9a7cd5ca91030
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-13 22:18:54 +0530

    Create util_scannerClass.java

---
+++ java/util_scannerClass.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;	
+class First
+{
+	public static void main(String args[])
+	{
+		Scanner s = new Scanner(System.in);
+		String name;
+		System.out.println("enter a name :");
+		name=s.nextLine();
+		System.out.println("name is :"+name);
+	}
+}		
