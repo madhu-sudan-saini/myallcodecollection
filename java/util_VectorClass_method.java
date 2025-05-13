19dfd37fef4a8edaa87c4977242701e06a2b96bf
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-13 22:40:35 +0530

    Create util_VectorClass_method.java

---
+++ java/util_VectorClass_method.java
@@ -0,0 +1,31 @@
+import java.util.Vector;	
+class First
+{
+	public static void main(String args[])
+	{
+		Vector v =new Vector();
+		System.out.println("size of vector list :"+v.size());
+		System.out.println("capacity of vector list :"+v.capacity());
+		System.out.println("vector list is isEmpty  :"+v.isEmpty());
+		
+		v.addElement(10);
+		v.addElement(20);
+		v.addElement(30);
+		v.addElement(40);
+		v.addElement(50);
+		v.addElement(60);
+		
+		System.out.println("size of vector list :"+v.size());
+		System.out.println("capacity of vector list :"+v.capacity());
+		System.out.println("vector list is isEmpty  :"+v.isEmpty());
+
+		System.out.println("first element of vector list :"+v.firstElement());
+	
+		System.out.println("last element of vector list :"+v.lastElement());
+	
+		Object ans=v.get(4);
+		System.out.println(" element of vector list in index :"+ans);
+
+	
+	}
+}		
