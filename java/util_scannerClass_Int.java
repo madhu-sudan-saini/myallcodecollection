bb06f6f293d485ec66ed30de3ea43753a029f76e
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-13 22:21:45 +0530

    Create util_scannerClass_Int.java

---
+++ java/util_scannerClass_Int.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;	
+class First
+{
+	public static void main(String args[])
+	{
+		Scanner s = new Scanner(System.in);
+		int a,b;
+		System.out.println("enter a number:");
+		a=s.nextInt();
+		b=s.nextInt();
+		int sum=a+b;
+		System.out.println("sum is  :"+sum);
+	}
+}		
