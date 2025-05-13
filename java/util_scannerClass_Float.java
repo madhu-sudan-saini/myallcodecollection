647b0f64ec40a1b728659efb4ef75ba1e6175edf
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-13 22:23:55 +0530

    Create util_scannerClass_Float.java

---
+++ java/util_scannerClass_Float.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;	
+class First
+{
+	public static void main(String args[])
+	{
+		Scanner s = new Scanner(System.in);
+		float a,b;
+		System.out.println("enter a number:");
+		a=s.nextFloat();
+		b=s.nextFloat();
+		float sum=a+b;
+		System.out.println("sum is  :"+sum);
+	}
+}		
