95f4eb33efed70f8997767506111f00844a0bfdb
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-13 22:44:42 +0530

    Create vectorClass_Unbounded_vector.java

---
+++ java/vectorClass_Unbounded_vector.java
@@ -0,0 +1,17 @@
+import java.util.Vector;	
+class First
+{
+	public static void main(String args[])
+	{
+		Vector v =new Vector();
+		
+		v.addElement(10);
+		v.addElement('a');
+		v.addElement(30.0);
+		v.addElement("kamal");
+		v.addElement(25.5);
+		v.addElement(60);
+		
+		System.out.println(v);
+	}
+}		
