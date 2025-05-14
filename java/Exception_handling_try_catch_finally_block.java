ef3186d3ce4f153cbd49eab662f0f3b0ccde34ed
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-14 22:51:28 +0530

    Create Exception_handling_try_catch_finally_block.java

---
+++ java/Exception_handling_try_catch_finally_block.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+class First 
+{
+	public static void main(String args[])
+	{
+		int a,b,ans=0;
+		try
+		{
+		a=Integer.parseInt(JOptionPane.showInputDialog("enter a number"));
+		b=Integer.parseInt(JOptionPane.showInputDialog("enter a number"));
+
+		 ans=a/b;
+		}
+		catch(Exception e)
+		{
+			System.out.println("error :"+e);
+		}
+		finally
+		{
+			System.out.println("we are in the catch block");
+		}	
+
+		
+		System.out.println("ans :"+ans);
+		System.out.println("end of main");
+	}
+}
