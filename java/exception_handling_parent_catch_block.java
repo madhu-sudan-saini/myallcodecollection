6feb14a87f7b33a6b2cd6cf731a1b3cf722063f0
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-14 22:46:15 +0530

    Create exception_handling_parent_catch_block.java

---
+++ java/exception_handling_parent_catch_block.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+class First 
+{
+	public static void main(String args[])
+	{
+		int a,b,ans=0;
+		try
+		{
+		a=Integer.parseInt(JOptionPane.showInputDialog("enter a number"));
+		b=Integer.parseInt(JOptionPane.showInputDialog("enter a number"));
+
+		 ans=a/b;
+		}
+		catch(Exception e)
+		{
+			System.out.println("error :"+e);
+		}	
+
+		
+		System.out.println("ans :"+ans);
+		System.out.println("end of main");
+	}
+}
