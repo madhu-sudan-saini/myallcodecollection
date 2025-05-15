80189b57b6197bb3b4c268fbf8dd5903d8b44a7b
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-15 22:42:58 +0530

    Create set_color_frame.java

---
+++ java/set_color_frame.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+class WinDemo extends Frame
+{
+	public WinDemo()
+	{
+		
+		setSize(400,400);
+		setTitle("eg of frame");
+		setBackground(Color.RED);
+		setVisible(true);
+	}
+}
+class Demo
+{
+	public static void main(String args[])
+	{
+		WinDemo obj =new WinDemo();
+	}
+}
+	
