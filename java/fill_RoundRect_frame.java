3661751525a88d4b0a88f22ccc27621de6325d0f
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-15 23:39:13 +0530

    Create fill_RoundRect_frame.java

---
+++ java/fill_RoundRect_frame.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+class WinDemo extends Frame
+{
+	public WinDemo()
+	{
+		
+		setSize(1000,1000);
+		setTitle("eg of frame");
+		setBackground(Color.RED);
+		setVisible(true);
+	}
+	public void paint(Graphics g)
+	{
+
+		g.setColor(Color.YELLOW);
+		g.fillRoundRect(200,200,400,400,40,40);
+		
+	}
+}
+class Demo
+{
+	public static void main(String args[])
+	{
+		WinDemo obj =new WinDemo();
+	}
+}
+	
