3ca84a8a6ae351a768dfb589889d9effea9de8a8
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-15 23:12:21 +0530

    Create draw_Rectangle_frameClass.java

---
+++ java/draw_Rectangle_frameClass.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+class WinDemo extends Frame
+{
+	public WinDemo()
+	{
+		
+		setSize(1000,1000);
+		setTitle("eg of frame");
+		setBackground(Color.RED);
+		setVisible(true);
+	}
+	public void paint(Graphics g)
+	{
+
+		g.setColor(Color.YELLOW);
+		g.drawRect(300,200,400,500);
+
+	}
+}
+class Demo
+{
+	public static void main(String args[])
+	{
+		WinDemo obj =new WinDemo();
+	}
+}
+	
