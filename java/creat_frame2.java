2c324abc4c3473bc4f066be1deae98e867f86455
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-15 22:40:21 +0530

    Create creat_frame2.java

---
+++ java/creat_frame2.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+class WinDemo extends Frame
+{
+	public WinDemo()
+	{
+		
+		setSize(400,400);
+		setTitle("eg of frame");
+		setVisible(true);
+	}
+}
+class Demo
+{
+	public static void main(String args[])
+	{
+		WinDemo obj =new WinDemo();
+	}
+}
+	
