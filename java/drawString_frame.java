1ea105eef55ba014e28d50200faf91592e22dc8e
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-15 22:52:40 +0530

    Create drawString_frame.java

---
+++ java/drawString_frame.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+class WinDemo extends Frame
+{
+	public WinDemo()
+	{
+		
+		setSize(1000,1000);
+		setTitle("eg of frame");
+		setBackground(Color.RED);
+		setVisible(true);
+	}
+	public void paint(Graphics g)
+	{
+		g.setColor(Color.YELLOW);
+		g.drawString("Hello World!",400,400);
+		
+
+	}
+}
+class Demo
+{
+	public static void main(String args[])
+	{
+		WinDemo obj =new WinDemo();
+	}
+}
+	
