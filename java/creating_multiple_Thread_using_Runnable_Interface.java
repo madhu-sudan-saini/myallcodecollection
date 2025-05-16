18e2961f5b058283521332d15a9d76b1ca05f2f4
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-16 23:49:16 +0530

    Create creating_multiple_Thread_using_Runnable_Interface.java

---
+++ java/creating_multiple_Thread_using_Runnable_Interface.java
@@ -0,0 +1,54 @@
+class MyThread implements Runnable
+{
+	private String name;
+	private Thread t;
+	public MyThread(String name)
+	{
+		this.name=name;
+		t=new Thread(this,name);
+		t.start();
+	}	
+	public void run()
+	{
+		int i;
+		for(i=0;i<=10;i++)
+		{	
+
+		try
+		{
+			System.out.println(Thread.currentThread().getName()+" "+i);
+			Thread.sleep(2000);
+		}
+		catch(Exception e)
+		{
+		
+		}
+		}
+	}
+}
+class Demo
+{
+	public static void main(String args[])
+	{
+		MyThread m1 =new MyThread("child1");
+		MyThread m2 =new MyThread("child2");
+		MyThread m3 =new MyThread("child3");
+		int i;
+		for(i=0;i<=10;i++)
+		{	
+
+		try
+		{
+			System.out.println(Thread.currentThread().getName()+" "+i);
+			Thread.sleep(2000);
+		}
+		catch(Exception e)
+		{
+		
+		}
+		}
+	}
+}
+		
+			
+	
