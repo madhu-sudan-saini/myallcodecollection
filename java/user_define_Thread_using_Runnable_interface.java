e112144c4564e6cb11c3c99b33456a25092969ad
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2025-05-16 23:44:38 +0530

    Create user_define_Thread_using_Runnable_interface.java

---
+++ java/user_define_Thread_using_Runnable_interface.java
@@ -0,0 +1,52 @@
+class MyThread implements Runnable
+{
+	private String name;
+	private Thread t;
+	public MyThread(String name)
+	{
+		this.name=name;
+		t=new Thread(this,name);
+		t.start();
+	}	
+	public void run()
+	{
+		int i;
+		for(i=0;i<=10;i++)
+		{	
+
+		try
+		{
+			System.out.println(Thread.currentThread().getName()+" "+i);
+			Thread.sleep(2000);
+		}
+		catch(Exception e)
+		{
+		
+		}
+		}
+	}
+}
+class Demo
+{
+	public static void main(String args[])
+	{
+		MyThread f =new MyThread("child");
+		int i;
+		for(i=0;i<=10;i++)
+		{	
+
+		try
+		{
+			System.out.println(Thread.currentThread().getName()+" "+i);
+			Thread.sleep(2000);
+		}
+		catch(Exception e)
+		{
+		
+		}
+		}
+	}
+}
+		
+			
+	
